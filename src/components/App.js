bb9ca3527eba82606bb8a24dbeae62a2d8f9ff20
Dawid Draguła <dawid.dragula@gmail.com>
2021-06-24 13:26:16 +0200

    Simple project structure

---
+++ src/components/App.js
@@ -0,0 +1,7 @@
+function App() {
+    return (
+        <div>This is the App</div>
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ThemeProvider from '@material-ui/styles/ThemeProvider';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import App from './components/App';
+import DarkTheme from './themes/dark.js';
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>Hello World!</div>
+    <ThemeProvider theme={DarkTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
